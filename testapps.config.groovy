5868256eef7258a67c0aeb19235b5609c77d912c
Dean Del Ponte <webmaster@bradleycorp.com>
2013-08-19 22:33:24 -0500

    Preparing the 1.0.RC5 branch

--- testapps.config.groovy
+++ testapps.config.groovy
@@ -1,4 +1,4 @@
-String version = '1.0.RC4-SNAPSHOT'
+String version = '1.0.RC5-SNAPSHOT'
 String grailsHomeRoot = '/path/to/your/grails/home'
 String dotGrailsCommon = '/path/to/.grails/in/your/home/directory'
 String projectDirCommon = '/path/to/platform-ui/plugin/code'
