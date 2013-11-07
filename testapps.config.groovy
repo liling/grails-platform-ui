c19cb4fe0673149847cc4cf888902fc745996df3
Dean Del Ponte <webmaster@bradleycorp.com>
2013-11-07 09:46:08 -0600

    #GPPLATUI-32: If I run prod run-war or war up the app and run it under Tomcat on a separate server, I get an error

--- testapps.config.groovy
+++ testapps.config.groovy
@@ -1,17 +1,7 @@
-String version = '1.0.RC6'
+String version = 'Version #'
 String grailsHomeRoot = '/path/to/your/grails/home'
 String dotGrailsCommon = '/path/to/.grails/in/your/home/directory'
 String projectDirCommon = '/path/to/target/directory/of/test/apps'
-
-v13 {
-    grailsVersion = '1.3.9'
-    pluginVersion = version
-    dotGrails = dotGrailsCommon
-    projectDir = projectDirCommon
-    grailsHome = grailsHomeRoot + '/' + grailsVersion
-    plugins = ''
-    dependencies = ''
-}
 
 v20 {
     grailsVersion = '2.0.4'
@@ -43,5 +33,13 @@ v22 {
                 exclude "spock-grails-support"
             }"""
     dependencies = 'test "org.spockframework:spock-grails-support:0.7-groovy-2.0"'
+}
+
+v23 {
+    grailsVersion = '2.3.1'
+    pluginVersion = version
+    dotGrails = dotGrailsCommon
+    projectDir = projectDirCommon
+    grailsHome = grailsHomeRoot + '/' + grailsVersion
 }
 
